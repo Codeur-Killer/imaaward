a7007b298d88c5e12226c336a3ff9ea556d76eb4
Codeur-Killer <elphenomenalo@gmail.com>
2026-05-14 22:49:54 +0200

    Mise à jour des lauréats

--- src/data/laureatesEdition1.js
+++ src/data/laureatesEdition1.js
@@ -1,10 +1,66 @@
 export const laureatesEdition1 = [
   {
     id: 1,
-    name: "Gomez",
-    category: "Meilleur Manager",
-    photo: "/Gomez.jpg", 
+    name: "Chewe",
+    category: "BEST MANAGER OF THE YEAR",
+    photo: "/chewe.jpeg", 
     description: "Vainqueur de la première édition."
   },
+
+  {
+    id: 2,
+    name: "Gomez",
+    category: "BEST FEMALE MANAGER",
+    photo: "/gomez.jpeg", 
+    description: "Vainqueur de la première édition."
+  },
+  {
+    id: 3,
+    name: "SWELL",
+    category: "BEST RAP IVOIR MANAGER",
+    photo: "/swell.jpeg", 
+    description: "Vainqueur de la première édition."
+  },
+
+  {
+    id: 4,
+    name: "FEMUA",
+    category: "BEST EVENT PROMOTER",
+    photo: "/femua.jpeg", 
+    description: "Vainqueur de la première édition."
+  },
+
+    {
+    id: 5,
+    name: "Emerson Manadji",
+    category: "BEST COUPE DECALE MANAGER",
+    photo: "/emerson.jpeg", 
+    description: "Vainqueur de la première édition."
+  },
+    {
+    id: 6,
+    name: "Chuck Well",
+    category: "BEST ART DIRECTOR",
+    photo: "/chuck.jpeg", 
+    description: "Vainqueur de la première édition."
+  },
+
+  {
+    id: 7,
+    name: "Arsène Florian",
+    category: "BEST ZOUGLOU MANAGER",
+    photo: "/arsene.jpeg", 
+    description: "Vainqueur de la première édition."
+  },
+
+  {
+    id: 8,
+    name: "Jamaica ",
+    category: "BEST VARIETY MANAGER",
+    photo: "/jamaica.jpeg", 
+    description: "Vainqueur de la première édition."
+  },
+
+ 
 
 ];
