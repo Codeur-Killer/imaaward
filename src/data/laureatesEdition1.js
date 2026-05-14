d79d7db8048cb99f173f6e3fa14ec9e39c5ffd0f
Codeur-Killer <elphenomenalo@gmail.com>
2026-05-14 21:56:43 +0200

    Mise à jour

--- src/data/laureatesEdition1.js
+++ src/data/laureatesEdition1.js
@@ -1,23 +1,10 @@
 export const laureatesEdition1 = [
   {
     id: 1,
-    name: "Nom du Lauréat 1",
-    category: "Meilleur Artiste",
-    photo: "/images/laureat1.jpg", // Remplacez par le vrai chemin ou URL
+    name: "Gomez",
+    category: "Meilleur Manager",
+    photo: "/Gomez.jpg", 
     description: "Vainqueur de la première édition."
   },
-  {
-    id: 2,
-    name: "Nom du Lauréat 2",
-    category: "Révélation de l'année",
-    photo: "/images/laureat2.jpg", // Remplacez par le vrai chemin ou URL
-    description: "Vainqueur de la première édition."
-  },
-  {
-    id: 3,
-    name: "Nom du Lauréat 3",
-    category: "Meilleur Clip Vidéo",
-    photo: "/images/laureat3.jpg", // Remplacez par le vrai chemin ou URL
-    description: "Vainqueur de la première édition."
-  }
+
 ];
